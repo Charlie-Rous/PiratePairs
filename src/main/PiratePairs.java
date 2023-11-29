f07ddcc24986ae8073b3ea3d6d28fd7017d09681
Charlie-Rous <crousmaniere@brooksschool.org>
2023-11-28 22:49:57 -0500

    add to player hand function

--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -1,11 +1,16 @@
 public class PiratePairs {
     static Player[] players = new Player[4];
+    static Player p1 = new Player("Dave");
     static Deck deck = new Deck();
     public static void main(String[] args){
-        deck.populateDeck();
-        deck.shuffle();
-        deck.printDeck();
-        System.out.println("Card: " + deck.dealCard());
-        deck.printDeck();
+        // deck.populateDeck();
+        // deck.shuffle();
+        // deck.printDeck();
+        // System.out.println("Card: " + deck.dealCard());
+        // deck.printDeck();
+
+        
+
+
     }
 }
