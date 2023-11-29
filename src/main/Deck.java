112aef6bbe069e5f7771c26db6022de52f971d9a
Charlie-Rous <crousmaniere@brooksschool.org>
2023-11-28 22:18:36 -0500

    Shuffle

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,9 +1,10 @@
 public class Deck {
-    int[] cards = new int[55];
+    final int DECK_LENGTH = 55;
+    int[] cards = new int[DECK_LENGTH];
 
     int index = 0;
 
-    int[] discard = new int[55];
+    int[] discard = new int[DECK_LENGTH];
 
    public void populateDeck() {
     for (int i = 1; i <= 10; i++) {
@@ -15,11 +16,24 @@ public class Deck {
    }
 
    public void printDeck() {
-    for (int i = 0; i < 55; i++) {
+    for (int i = 0; i < DECK_LENGTH; i++) {
         System.out.println(cards[i]);
     }
    }
 
+   public void shuffle() {
+    int[] shuffledCards = new int[DECK_LENGTH];
+    int index = 0;
+    for (int i = DECK_LENGTH - 1; i >= 0; i--) {
+        int randomCardIndex = (int) (Math.random() * i);
+        shuffledCards[index] = cards[randomCardIndex];
+
+        int tempCard = cards[randomCardIndex];
+        cards[randomCardIndex] = cards[i];
+        cards[i] = tempCard;
+        index++;
+    }
+   }
     
 
     
--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -2,6 +2,7 @@ public class PiratePairs {
     public static void main(String[] args){
         Deck deck = new Deck();
         deck.populateDeck();
+        deck.shuffle();
         deck.printDeck();
     }
 }
