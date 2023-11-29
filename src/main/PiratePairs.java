e420c5edbfac071695bcde3df4037ad9448f4004
Charlie-Rous <crousmaniere@brooksschool.org>
2023-11-28 22:57:37 -0500

    set up start of game

--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -1,16 +1,21 @@
 public class PiratePairs {
-    static Player[] players = new Player[4];
+    static Player[] players = new Player[2];
     static Player p1 = new Player("Dave");
+    static Player p2 = new Player("Mark");
+    
     static Deck deck = new Deck();
     public static void main(String[] args){
-        // deck.populateDeck();
-        // deck.shuffle();
-        // deck.printDeck();
-        // System.out.println("Card: " + deck.dealCard());
-        // deck.printDeck();
-
+        players[0] = p1;
+        players[1] = p2;
+        Start();
         
+    }
 
-
+    public static void Start() {
+        deck.populateDeck();
+        deck.shuffle();
+        for (Player player : players) {
+            player.addToHand(deck.dealCard());
+        }
     }
 }
