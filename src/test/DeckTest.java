c4940dfb845e34cdb3d41a0e573bb76e57f8d0ba
Charlie-Rous <crousmaniere@brooksschool.org>
2023-11-30 13:57:31 -0500

    Working game

---
+++ src/test/DeckTest.java
@@ -0,0 +1,10 @@
+package src.test;
+import java.util.Arrays;
+import src.main.Deck;
+
+public class DeckTest {
+    static Deck deck = new Deck();
+    public static void main(String[] args) {
+
+    }
+}
